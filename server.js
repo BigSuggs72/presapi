2fa4073fbb60cedf007efcedbb0474fbc73b80f0
BigSuggs72 <msofi72@gmail.com>
2022-06-14 16:21:13 -0400

    update

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const cors = require('cors')
 const PORT = 8000
 require('dotenv').config()
 
-app.use(cors())
 
+//SET MIDDLEWARE
+app.set('view engine', 'ejs')
+app.use(cors())
+// app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+//Objects
 const presidents = {
     'unknown' : {
         'number' : 'unknown',
