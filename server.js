32a678a1bc2b79c43b961c757ad561bf5d4b3c5c
BigSuggs72 <msofi72@gmail.com>
2022-08-04 20:51:16 -0400

    update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ MongoClient.connect(dbConnectionStr)
 //SET MIDDLEWARE
 app.set('view engine', 'ejs')
 app.use(cors({
-    origin: '*'
+    origin: 'Access-Control-Allow-Origin: *'
 }))
 // app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
