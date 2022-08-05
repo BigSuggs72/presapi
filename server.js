cff70e7fa5c07ecc730539fc844ed3c0ca2d7c37
BigSuggs72 <msofi72@gmail.com>
2022-08-04 20:57:21 -0400

    update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ MongoClient.connect(dbConnectionStr)
 //SET MIDDLEWARE
 app.set('view engine', 'ejs')
 app.use(cors({
-    origin: 'Access-Control-Allow-Origin: *'
+    origin: 'https://presapi.herokuapp.com/*'
 }))
 // app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
